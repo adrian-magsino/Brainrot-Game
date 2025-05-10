4bbe0f1e1c101590418afff0ed5ef5c4f5f38112
Peter Alvir <alvirgonzales16@gmail.com>
2025-05-10 18:55:57 +0800

    Created JoinmenuScene

--- Scripts/UI/mainmenu_scene.gd
+++ Scripts/UI/mainmenu_scene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Gameplay/GameplayScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/UI/JoinmenuScene.tscn")
 
 func _on_settings_pressed() -> void:
 	#get_tree().change_scene_to_file()

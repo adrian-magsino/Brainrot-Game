43fa5e37e5500d51a8ae94cdaa591d618f57ff72
Peter Alvir <alvirgonzales16@gmail.com>
2025-05-12 18:51:11 +0800

    Added Join list Scene

--- Scripts/UI/joinmenu_scene.gd
+++ Scripts/UI/joinmenu_scene.gd
@@ -4,7 +4,7 @@ func _on_create_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_join_game_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/UI/JoinlistScene.tscn")
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/MainmenuScene.tscn")

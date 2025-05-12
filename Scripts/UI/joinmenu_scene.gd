f2e3bbf70dbd93c9521f9cd8257678cd258ff583
Peter Alvir <alvirgonzales16@gmail.com>
2025-05-13 01:05:18 +0800

    Added Lobby Scene

--- Scripts/UI/joinmenu_scene.gd
+++ Scripts/UI/joinmenu_scene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_create_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/UI/LobbyScene.tscn")
 
 func _on_join_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/JoinlistScene.tscn")
---
+++ Scripts/UI/lobby_scene.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var selectmode: Control = $Selectmode
+@onready var selectmap: Control = $Selectmap
+
+
+func _on_select_map_pressed() -> void:
+	selectmap.visible = true
+
+func _on_select_mode_pressed() -> void:
+	selectmode.visible = true
+
+func _on_back_to_lobby_pressed() -> void:
+	selectmode.visible = false
+	selectmap.visible = false
+
+func _on_back_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/JoinmenuScene.tscn")

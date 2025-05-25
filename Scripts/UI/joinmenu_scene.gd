542165bfdf6b55c5c3988fb066caab788c3c9107
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-05-25 20:45:37 +0800

    connected UI and gameplay

--- Scripts/UI/joinmenu_scene.gd
+++ Scripts/UI/joinmenu_scene.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_escape_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Gameplay/BotTestingScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Gameplay/NavigationTestScene.tscn")
 
 func _on_survival_pressed() -> void:
 	pass # Replace with function body.

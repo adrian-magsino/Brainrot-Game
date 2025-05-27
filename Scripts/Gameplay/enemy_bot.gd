16287e1897f493d973e2d538f482f61c3d1e46b3
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-05-28 01:15:06 +0800

    refactored bot damage system

--- Scripts/Gameplay/enemy_bot.gd
+++ Scripts/Gameplay/enemy_bot.gd
@@ -1,27 +1,25 @@
-extends DamageableEntity
+extends CharacterBody2D
 
 @export var move_speed: float = 200.0
 @export var bot_name: String = "ENEMY BOT"
-@export var damage: int = 10
-@export var damage_cooldown: float = 1.0
+@export var attack_cooldown: float = 1.0
+@export var can_destroy_objects: bool = false
 
 @onready var target_player = get_parent().get_node("PLAYER")
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
-@onready var dammage_cooldown_timer := $DamageCooldownTimer
-@onready var damage_area := $DamageArea
+@onready var attack_cooldown_timer := $AttackCooldownTimer
+@onready var attack_area: Area2D = $AttackArea
 @onready var sfx_bot: AudioStreamPlayer2D = $SFX_bot
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var attack_component: AttackComponent = $AttackComponent
 
-var can_damage: bool = true
-var player_in_range: DamageableEntity = null
+var can_attack: bool = true
+var player_in_range: Player = null
 
 func _ready() -> void:
 	sfx_bot.play()
-	current_health = max_health
-	update_health_bar()
-	health_bar.visible = false
-	if hide_healthbar_timer:
-		hide_healthbar_timer.timeout.connect(_on_hide_timer_timeout)
-	dammage_cooldown_timer.wait_time = damage_cooldown
+	health_component.update_health_bar()
+	attack_cooldown_timer.wait_time = attack_cooldown
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector3()
@@ -36,22 +34,28 @@ func make_path() -> void:
 func _on_timer_timeout() -> void:
 	make_path()
 
-func _on_damage_area_body_entered(body: Node2D) -> void:
-	if body is Player:
-		player_in_range = body
-		deal_damage()
-
-func _on_damage_area_body_exited(body: Node2D) -> void:
-	if body == player_in_range:
+func _on_attack_area_area_entered(area: Area2D) -> void:
+	if area is HitboxComponent and area.get_parent() != self:
+		var player = area.get_parent()
+		if player is Player and not player.is_dead:
+			player_in_range = player
+			if can_attack:
+				_attack_player()
+			#player_in_range = area.get_parent()
+			#_attack()
+	
+func _on_attack_area_area_exited(area: Area2D) -> void:
+	if area is HitboxComponent and area.get_parent() == player_in_range:
 		player_in_range = null
 
 func _on_damage_cooldown_timer_timeout() -> void:
-	can_damage = true
-	deal_damage()
+	can_attack = true
+	if player_in_range:
+		_attack_player()
+
+func _attack_player():
+	if player_in_range and can_attack:
+		player_in_range.get_node("HitboxComponent").take_damage(attack_component)
+		can_attack = false
+		attack_cooldown_timer.start()
 	
-func deal_damage():
-	if can_damage and player_in_range and not player_in_range.is_dead:
-		print("DEAL DAMAGE")
-		player_in_range.take_damage(damage, self)
-		can_damage = false
-		dammage_cooldown_timer.start()

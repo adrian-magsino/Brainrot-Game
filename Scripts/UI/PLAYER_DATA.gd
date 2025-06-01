92336217bfafc7a3eec3181fee9477956b831399
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-06-01 16:24:09 +0800

    changed JSON file path

--- Scripts/UI/PLAYER_DATA.gd
+++ Scripts/UI/PLAYER_DATA.gd
@@ -10,16 +10,17 @@ func save_game():
 		"player_name": player_name,
 		"current_character": current_character
 	}
-	var file = FileAccess.open("user://save_data.json", FileAccess.WRITE)
+	var file = FileAccess.open("res://User data/save_data.json", FileAccess.WRITE)
 	file.store_string(JSON.stringify(save_data))
 	file.close()
-
+	print("SAVED PLAYER DATA")
 func load_game():
-	if FileAccess.file_exists("user://save_data.json"):
-		var file = FileAccess.open("user://save_data.json", FileAccess.READ)
+	if FileAccess.file_exists("res://User data/save_data.json"):
+		var file = FileAccess.open("res://User data/save_data.json", FileAccess.READ)
 		var data = JSON.parse_string(file.get_as_text())
 		if typeof(data) == TYPE_DICTIONARY:
 			total_kills = data.get("total_kills", 0)
 			player_name = data.get("player_name", "")
 			current_character = data.get("current_character", "")
 		file.close()
+		print("LOADED PLAYER DATA")
--- Scripts/UI/mainmenu_scene.gd
+++ Scripts/UI/mainmenu_scene.gd
@@ -1,14 +1,14 @@
 extends Control
 
 func _ready() -> void:
-	Global.load_game()  # Load game data here
+	PLAYER_DATA.load_game()  # Load game data here
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/ModemenuScene.tscn")
 
 func _on_settings_pressed() -> void:
 	#get_tree().change_scene_to_file()
-	pass
+	PLAYER_DATA.save_game()
 	
 func _on_credits_pressed() -> void:
 	#get_tree().change_scene_to_file()

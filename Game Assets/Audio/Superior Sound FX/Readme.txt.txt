7c26d3f5c43c51047b90e9af2554523eae905b48
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-06-02 23:27:57 +0800

    added explosive barrel assets

---
+++ Game Assets/Audio/Superior Sound FX/Readme.txt.txt
@@ -0,0 +1,11 @@
+Superior Sound FX: Lasers and Explosions
+
+Twenty-six free high quality sound effects (WAV, 44.1 kHz, 16-bit).
+
+    Explosions
+    Gunshots
+    Laser blasts
+    Assorted sci-fi sound effects
+
+
+These sounds are free to use in all projects with no cost. It would be nice if you give credit to "Soiree Games (soiree.info)" 
---
+++ Scenes/Gameplay/Interactable_Objects/Explosive Barrel/explosive_barrel.gd
@@ -0,0 +1,50 @@
+extends StaticBody2D
+
+#@export var explosion_damage: int = 50
+@export var explosion_radius: float = 100.0
+@export var ExplosionParticles: PackedScene
+
+@onready var explosion_area = $ExplosionArea
+@onready var attack_component = $AttackComponent
+
+#ASSETS
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sfx_explosion: AudioStreamPlayer2D = $SFX_explosion
+
+var is_destroyed: bool = false
+
+func destroy(attack: AttackComponent):
+	if is_destroyed:
+		return
+	is_destroyed = true
+	#The destroyer of this explosive
+	attack_component.attacker = attack.attacker 
+	print("DESTROYER: ", attack_component.attacker)
+	# Damage nearby bodies
+	apply_explosion_damage_all(attack_component)
+	
+	play_explosion_animation()
+	
+	await animated_sprite_2d.animation_finished
+	queue_free()
+
+
+func apply_explosion_damage_all(attack: AttackComponent):
+	if not attack:
+		return
+	explosion_area.monitoring = true
+	for area in explosion_area.get_overlapping_areas():
+		if area is HitboxComponent and area != get_node("HitboxComponent"):
+			area.take_damage(attack)
+	explosion_area.monitoring = false
+
+func play_explosion_animation():
+	sfx_explosion.play()
+	animated_sprite_2d.play("explode")
+	var _particle = ExplosionParticles.instantiate()
+	_particle.position = global_position
+	_particle.rotation = global_rotation
+	_particle.emitting = true
+	get_tree().current_scene.add_child(_particle)
+	
+	#animated_sprite_2d.animation_finished.connect(_on_animated_sprite_2d_animation_finished)

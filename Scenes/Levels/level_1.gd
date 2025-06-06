f4284e3b836625b895260ff1893cb37bcf4616aa
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-06-06 21:07:01 +0800

    added a new win condition for levels

--- Scenes/Levels/level_1.gd
+++ Scenes/Levels/level_1.gd
@@ -2,6 +2,26 @@ extends Level_Manager
 
 const level: String = "lvl1"
 
+@export var required_kills: int = 5
+@onready var exit_portal: Node2D = $ExitPortal
+@onready var portal_interactable_component: Area2D = exit_portal.get_node("Interactable")
+const win_condition = "Kill %d enemies to open the portal"
+
+func _ready() -> void:
+	exit_portal.visible = false
+	portal_interactable_component.set_deferred("monitorable", false)
+	portal_interactable_component.set_deferred("monitoring", false)
+	
+func check_win_condition():
+	if enemies_killed >= required_kills:
+		exit_portal.visible = true
+		portal_interactable_component.set_deferred("monitorable", true)
+		portal_interactable_component.set_deferred("monitoring", true)
+	
+func register_enemy_kill(attacker: Node) -> void:
+	super.register_enemy_kill(attacker)
+	check_win_condition()
+
 func game_victory():
 	super.game_victory()
 	save_level_progress()

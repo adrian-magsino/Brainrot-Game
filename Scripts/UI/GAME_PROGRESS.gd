b825380119db0c1989c9992699d1ae72d9f5a182
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-06-16 06:05:05 +0800

    fixed json file paths

--- Scripts/UI/GAME_PROGRESS.gd
+++ Scripts/UI/GAME_PROGRESS.gd
@@ -3,24 +3,24 @@ extends Node
 var level_completed = {
 	"lvl1": false,
 	"lvl2": false,
-	"lvl3": false,
-	"lvl4": false,
-	"lvl5": false,
-	"lvl6": false,
-	"lvl7": false,
-	"lvl8": false,
-	"lvl9": false,
-	"lvl10": false
+	"lvl3": true,
+	"lvl4": true,
+	"lvl5": true,
+	"lvl6": true,
+	"lvl7": true,
+	"lvl8": true,
+	"lvl9": true,
+	"lvl10": true
 }
 
 func save_progress():
-	var file = FileAccess.open("res://User data/game_progress.json", FileAccess.WRITE)
+	var file = FileAccess.open("user://game_progress.json", FileAccess.WRITE)
 	file.store_string(JSON.stringify(level_completed))
 	file.close()
 
 func load_progress():
-	if FileAccess.file_exists("res://User data/game_progress.json"):
-		var file = FileAccess.open("res://User data/game_progress.json", FileAccess.READ)
+	if FileAccess.file_exists("user://game_progress.json"):
+		var file = FileAccess.open("user://game_progress.json", FileAccess.READ)
 		var loaded_data = JSON.parse_string(file.get_as_text())
 		if typeof(loaded_data) == TYPE_DICTIONARY:
 			level_completed = loaded_data

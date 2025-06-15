a6607c7abfe4b3795c785185c12ebcdda277b46d
javarice-plus-plus <cs.adrian.magsino@gmail.com>
2025-06-15 21:30:28 +0800

    added lighting management

--- Scenes/Levels/objectives_FindPortal.gd
+++ Scenes/Levels/objectives_FindPortal.gd
@@ -1,12 +1,17 @@
 extends Level_Manager
 
 @export var level_id: int
+@export var require_lighting: bool
 
 func _ready() -> void:
 	Bgm.stop()
 	PLAYER.update_player_lives()
 	level_objectives = "FIND THE PORTAL"
 	update_objectives_display(level_objectives)
+	if require_lighting:
+		LightingManager.set_lighting_enabled(true)
+	else:
+		LightingManager.set_lighting_enabled(false)
 
 func game_victory():
 	super.game_victory()
